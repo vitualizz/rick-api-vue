0035e9d4744cab8ce93dfbbe439eecb2bb904322
Lee Palacios <vitualizz@gmail.com>
2020-11-14 02:08:10 -0500

    use api rick and morty

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,53 @@
 <template>
   <div>
-    <h1>
-      ¡Funciona!
-    </h1>
+    <table>
+      <tr>
+        <td>Nombres</td>
+      </tr>
+      <tr
+        v-for='(character, index) in characters'
+        :key='index'
+      >
+        <td>{{ character.name }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  data () {
+    return {
+      characters: []
+    }
+  },
+  mounted () {
+    this.getCharacters()
+  },
+  methods: {
+    async getCharacters () {
+      const url = 'https://rickandmortyapi.com/api/character'
+      const { data } = await this.axios.get(url)
+      this.characters = data.results
+    }
+  }
 }
 </script>
 
+<style scoped>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+table tr:first-child {
+  background: #eaeaea;
+}
+
+td {
+  padding: 10px;
+  min-width: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-const app = createApp(App).mount('#app')
+const app = createApp(App)
 app.use(VueAxios, axios)
+app.mount('#app')
